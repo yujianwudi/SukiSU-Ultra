340a94ef3018fad4ea4861c3a5f1e00f8b83d8e1
TwinbornPlate75 <42514046+TwinbornPlate75@users.noreply.github.com>
2025-07-08 22:34:01 +0800

    kernel/throne_tracker: Fix missing variable in function `my_actor` (#266)

--- kernel/throne_tracker.c
+++ kernel/throne_tracker.c
@@ -184,7 +184,7 @@ FILLDIR_RETURN_TYPE my_actor(struct dir_context *ctx, const char *name,
 		list_add_tail(&data->list, my_ctx->data_path_list);
 	} else {
 		if ((namelen == 8) && (strncmp(name, "base.apk", namelen) == 0)) {
-			struct apk_path_hash *pos;
+			struct apk_path_hash *pos, *n;
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 8, 0)
 			unsigned int hash = full_name_hash(dirpath, strlen(dirpath));
 #else
