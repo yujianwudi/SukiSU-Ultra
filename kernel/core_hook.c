a9e3c1cc8f410a20ba9f6fd3a1d2e34804355316
ShirkNeko <109797057+ShirkNeko@users.noreply.github.com>
2025-08-03 22:51:38 +0800

    kermel: Refactoring and separating dynamic signature-related structures and functions and implement hot-signature updates

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -46,7 +46,6 @@
 #include "manager.h"
 #include "selinux/selinux.h"
 #include "throne_tracker.h"
-#include "throne_tracker.h"
 #include "kernel_compat.h"
 #include "kpm/kpm.h"
 
@@ -406,7 +405,6 @@ int ksu_handle_prctl(int option, unsigned long arg2, unsigned long arg3,
 				on_post_fs_data();
 				// Initializing Dynamic Signatures
         		ksu_dynamic_sign_init();
-        		ksu_load_dynamic_sign();
         		pr_info("Dynamic sign config loaded during post-fs-data\n");
 			}
 			break;
