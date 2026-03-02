yapixel/KernelSU

manager: Refactor ViewModels with repository pattern and StateFlow (#3241)
This commit introduces a major refactoring of the manager's data and
state management by implementing the repository pattern and migrating
from mutable `State` objects to `StateFlow` and `UiState` data classes
in the ViewModels.

manager/app/src/main/java/me/weishu/kernelsu/data/model/RepoModule.kt
@@ -1,0 +1,33 @@
+package me.weishu.kernelsu.data.model
+
+import androidx.compose.runtime.Immutable
+
+@Immutable
+data class Author(
+    val name: String,
+    val link: String,
+)
+
+@Immutable
+data class ReleaseAsset(
+    val name: String,
+    val downloadUrl: String,
+    val size: Long
+)
+
+@Immutable
+data class RepoModule(
+    val moduleId: String,
+    val moduleName: String,
+    val authors: String,
+    val authorList: List<Author>,
+    val summary: String,
+    val metamodule: Boolean,
+    val stargazerCount: Int,
+    val updatedAt: String,
+    val createdAt: String,
+    val latestRelease: String,
+    val latestReleaseTime: String,
+    val latestVersionCode: Int,
+    val latestAsset: ReleaseAsset?,
+)

manager/app/src/main/java/me/weishu/kernelsu/ui/viewmodel/ModuleUiState.kt
@@ -1,0 +1,17 @@
+package me.weishu.kernelsu.ui.viewmodel
+
+import me.weishu.kernelsu.data.model.Module
+import me.weishu.kernelsu.data.model.ModuleUpdateInfo
+import me.weishu.kernelsu.ui.component.SearchStatus
+
+data class ModuleUiState(
+    val isRefreshing: Boolean = false,
+    val modules: List<Module> = emptyList(),
+    val moduleList: List<Module> = emptyList(),
+    val updateInfo: Map<String, ModuleUpdateInfo> = emptyMap(),
+    val searchStatus: SearchStatus = SearchStatus(""),
+    val searchResults: List<Module> = emptyList(),
+    val sortEnabledFirst: Boolean = false,
+    val sortActionFirst: Boolean = false,
+    val checkModuleUpdate: Boolean = true
+)

manager/app/src/main/java/me/weishu/kernelsu/ui/viewmodel/SettingsViewModel.kt
@@ -1,0 +1,168 @@
+package me.weishu.kernelsu.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import me.weishu.kernelsu.data.repository.SettingsRepository
+import me.weishu.kernelsu.data.repository.SettingsRepositoryImpl
+
+class SettingsViewModel(
+    private val repo: SettingsRepository = SettingsRepositoryImpl()
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(SettingsUiState())
+    val uiState: StateFlow<SettingsUiState> = _uiState.asStateFlow()
+
+    init {
+        refresh()
+    }
+
+    fun refresh() {
+        viewModelScope.launch {
+            val checkUpdate = repo.checkUpdate
+            val checkModuleUpdate = repo.checkModuleUpdate
+            val themeMode = repo.themeMode
+            val keyColor = repo.keyColor
+            val enablePredictiveBack = repo.enablePredictiveBack
+            val enableBlur = repo.enableBlur
+            val enableFloatingBottomBar = repo.enableFloatingBottomBar
+            val enableFloatingBottomBarBlur = repo.enableFloatingBottomBarBlur
+            val pageScale = repo.pageScale
+            val enableWebDebugging = repo.enableWebDebugging
+            val isLkmMode = repo.isLkmMode()
+
+            // Async loading for natives/features
+            val suCompatStatus = repo.getSuCompatStatus()
+            val suCompatPersistValue = repo.getSuCompatPersistValue()
+            val isSuEnabled = repo.isSuEnabled()
+
+            val suCompatMode = if (suCompatPersistValue == 0L) 2 else if (!isSuEnabled) 1 else 0
+
+            val kernelUmountStatus = repo.getKernelUmountStatus()
+            val isKernelUmountEnabled = repo.isKernelUmountEnabled()
+            val isDefaultUmountModules = repo.isDefaultUmountModules()
+
+            _uiState.update {
+                it.copy(
+                    checkUpdate = checkUpdate,
+                    checkModuleUpdate = checkModuleUpdate,
+                    themeMode = themeMode,
+                    keyColor = keyColor,
+                    enablePredictiveBack = enablePredictiveBack,
+                    enableBlur = enableBlur,
+                    enableFloatingBottomBar = enableFloatingBottomBar,
+                    enableFloatingBottomBarBlur = enableFloatingBottomBarBlur,
+                    pageScale = pageScale,
+                    enableWebDebugging = enableWebDebugging,
+                    suCompatStatus = suCompatStatus,
+                    suCompatMode = suCompatMode,
+                    isSuEnabled = isSuEnabled,
+                    kernelUmountStatus = kernelUmountStatus,
+                    isKernelUmountEnabled = isKernelUmountEnabled,
+                    isDefaultUmountModules = isDefaultUmountModules,
+                    isLkmMode = isLkmMode
+                )
+            }
+        }
+    }
+
+    fun setCheckUpdate(enabled: Boolean) {
+        repo.checkUpdate = enabled
+        _uiState.update { it.copy(checkUpdate = enabled) }
+    }
+
+    fun setCheckModuleUpdate(enabled: Boolean) {
+        repo.checkModuleUpdate = enabled
+        _uiState.update { it.copy(checkModuleUpdate = enabled) }
+    }
+
+    fun setThemeMode(mode: Int) {
+        repo.themeMode = mode
+        _uiState.update { it.copy(themeMode = mode) }
+    }
+
+    fun setKeyColor(color: Int) {
+        repo.keyColor = color
+        _uiState.update { it.copy(keyColor = color) }
+    }
+
+    fun setEnablePredictiveBack(enabled: Boolean) {
+        repo.enablePredictiveBack = enabled
+        _uiState.update { it.copy(enablePredictiveBack = enabled) }
+    }
+
+    fun setEnableBlur(enabled: Boolean) {
+        repo.enableBlur = enabled
+        _uiState.update { it.copy(enableBlur = enabled) }
+    }
+
+    fun setEnableFloatingBottomBar(enabled: Boolean) {
+        repo.enableFloatingBottomBar = enabled
+        _uiState.update { it.copy(enableFloatingBottomBar = enabled) }
+    }
+
+    fun setEnableFloatingBottomBarBlur(enabled: Boolean) {
+        repo.enableFloatingBottomBarBlur = enabled
+        _uiState.update { it.copy(enableFloatingBottomBarBlur = enabled) }
+    }
+
+    fun setPageScale(scale: Float) {
+        repo.pageScale = scale
+        _uiState.update { it.copy(pageScale = scale) }
+    }
+
+    fun setEnableWebDebugging(enabled: Boolean) {
+        repo.enableWebDebugging = enabled
+        _uiState.update { it.copy(enableWebDebugging = enabled) }
+    }
+
+    fun setSuCompatMode(mode: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+            when (mode) {
+                0 -> if (repo.setSuEnabled(true)) {
+                    repo.execKsudFeatureSave()
+                    repo.setSuCompatModePref(0)
+                    _uiState.update { it.copy(suCompatMode = 0, isSuEnabled = true) }
+                }
+
+                1 -> if (repo.setSuEnabled(true)) {
+                    repo.execKsudFeatureSave()
+                    if (repo.setSuEnabled(false)) {
+                        // "Disable until reboot" implies it should be enabled on next boot.
+                        // We set the preference to 0 (Enabled) to match the persistent state.
+                        repo.setSuCompatModePref(0)
+                        _uiState.update { it.copy(suCompatMode = 1, isSuEnabled = false) }
+                    }
+                }
+
+                2 -> if (repo.setSuEnabled(false)) {
+                    repo.execKsudFeatureSave()
+                    repo.setSuCompatModePref(2)
+                    _uiState.update { it.copy(suCompatMode = 2, isSuEnabled = false) }
+                }
+            }
+        }
+    }
+
+    fun setKernelUmountEnabled(enabled: Boolean) {
+        viewModelScope.launch(Dispatchers.IO) {
+            if (repo.setKernelUmountEnabled(enabled)) {
+                repo.execKsudFeatureSave()
+                _uiState.update { it.copy(isKernelUmountEnabled = enabled) }
+            }
+        }
+    }
+
+    fun setDefaultUmountModules(enabled: Boolean) {
+        viewModelScope.launch(Dispatchers.IO) {
+            if (repo.setDefaultUmountModules(enabled)) {
+                _uiState.update { it.copy(isDefaultUmountModules = enabled) }
+            }
+        }
+    }
+}

manager/app/src/main/java/me/weishu/kernelsu/ui/viewmodel/SuperUserUiState.kt
@@ -1,0 +1,28 @@
+package me.weishu.kernelsu.ui.viewmodel
+
+import androidx.compose.runtime.Immutable
+import me.weishu.kernelsu.data.model.AppInfo
+import me.weishu.kernelsu.ui.component.SearchStatus
+
+@Immutable
+data class GroupedApps(
+    val uid: Int,
+    val apps: List<AppInfo>,
+    val primary: AppInfo,
+    val anyAllowSu: Boolean,
+    val anyCustom: Boolean,
+    val shouldUmount: Boolean,
+    val ownerName: String? = null,
+)
+
+data class SuperUserUiState(
+    val isRefreshing: Boolean = false,
+    val appList: List<AppInfo> = emptyList(),
+    val groupedApps: List<GroupedApps> = emptyList(),
+    val userIds: List<Int> = emptyList(),
+    val searchStatus: SearchStatus = SearchStatus(""),
+    val searchResults: List<AppInfo> = emptyList(),
+    val showSystemApps: Boolean = false,
+    val showOnlyPrimaryUserApps: Boolean = false,
+    val error: Throwable? = null
+)

manager/app/src/main/java/me/weishu/kernelsu/ui/viewmodel/TemplateUiState.kt
@@ -1,0 +1,10 @@
+package me.weishu.kernelsu.ui.viewmodel
+
+import me.weishu.kernelsu.data.model.TemplateInfo
+
+data class TemplateUiState(
+    val isRefreshing: Boolean = false,
+    val templates: List<TemplateInfo> = emptyList(),
+    val templateList: List<TemplateInfo> = emptyList(),
+    val error: Throwable? = null
+)

manager/app/src/main/java/me/weishu/kernelsu/ui/webui/WebViewHelper.kt
@@ -20,8 +20,8 @@ import androidx.webkit.WebViewAssetLoader
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import me.weishu.kernelsu.R
+import me.weishu.kernelsu.data.repository.ModuleRepositoryImpl
 import me.weishu.kernelsu.ui.util.createRootShell
-import me.weishu.kernelsu.ui.viewmodel.ModuleViewModel
 import me.weishu.kernelsu.ui.viewmodel.SuperUserViewModel
 import java.io.File
 
@@ -33,12 +33,9 @@ internal suspend fun prepareWebView(
     webUIState: WebUIState,
 ) {
     withContext(Dispatchers.IO) {
-        val viewModel = ModuleViewModel()
-        if (viewModel.moduleList.isEmpty()) {
-            viewModel.loadModuleList()
-        }
-
-        val moduleInfo = viewModel.moduleList.find { info -> info.id == moduleId }
+        val repo = ModuleRepositoryImpl()
+        val modules = repo.getModules().getOrDefault(emptyList())
+        val moduleInfo = modules.find { info -> info.id == moduleId }
 
         if (moduleInfo == null) {
             withContext(Dispatchers.Main) {
@@ -91,7 +88,12 @@ internal suspend fun prepareWebView(
                 .setDomain("mui.kernelsu.org")
                 .addPathHandler(
                     "/",
-                    SuFilePathHandler(activity, webRoot, shell, { webUIState.currentInsets }, { enable -> webUIState.isInsetsEnabled = enable })
+                    SuFilePathHandler(
+                        activity,
+                        webRoot,
+                        shell,
+                        { webUIState.currentInsets },
+                        { enable -> webUIState.isInsetsEnabled = enable })
                 )
                 .build()
 
@@ -134,7 +136,13 @@ internal suspend fun prepareWebView(
                     return true
                 }
 
-                override fun onJsPrompt(view: WebView?, url: String?, message: String?, defaultValue: String?, result: JsPromptResult?): Boolean {
+                override fun onJsPrompt(
+                    view: WebView?,
+                    url: String?,
+                    message: String?,
+                    defaultValue: String?,
+                    result: JsPromptResult?
+                ): Boolean {
                     if (message == null || result == null || defaultValue == null) return false
                     webUIState.uiEvent = WebUIEvent.ShowPrompt(message, defaultValue, result)
                     return true

yujianwudi/SukiSU-Ultra

manager/app/src/main/java/com/sukisu/ultra/data/model/RepoModule.kt
@@ -1,0 +1,33 @@
+package com.sukisu.ultra.data.model
+
+import androidx.compose.runtime.Immutable
+
+@Immutable
+data class Author(
+    val name: String,
+    val link: String,
+)
+
+@Immutable
+data class ReleaseAsset(
+    val name: String,
+    val downloadUrl: String,
+    val size: Long
+)
+
+@Immutable
+data class RepoModule(
+    val moduleId: String,
+    val moduleName: String,
+    val authors: String,
+    val authorList: List<Author>,
+    val summary: String,
+    val metamodule: Boolean,
+    val stargazerCount: Int,
+    val updatedAt: String,
+    val createdAt: String,
+    val latestRelease: String,
+    val latestReleaseTime: String,
+    val latestVersionCode: Int,
+    val latestAsset: ReleaseAsset?,
+)

manager/app/src/main/java/com/sukisu/ultra/ui/viewmodel/ModuleUiState.kt
@@ -1,0 +1,17 @@
+package com.sukisu.ultra.ui.viewmodel
+
+import com.sukisu.ultra.data.model.Module
+import com.sukisu.ultra.data.model.ModuleUpdateInfo
+import com.sukisu.ultra.ui.component.SearchStatus
+
+data class ModuleUiState(
+    val isRefreshing: Boolean = false,
+    val modules: List<Module> = emptyList(),
+    val moduleList: List<Module> = emptyList(),
+    val updateInfo: Map<String, ModuleUpdateInfo> = emptyMap(),
+    val searchStatus: SearchStatus = SearchStatus(""),
+    val searchResults: List<Module> = emptyList(),
+    val sortEnabledFirst: Boolean = false,
+    val sortActionFirst: Boolean = false,
+    val checkModuleUpdate: Boolean = true
+)

manager/app/src/main/java/com/sukisu/ultra/ui/viewmodel/SettingsViewModel.kt
@@ -1,0 +1,174 @@
+package com.sukisu.ultra.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import com.sukisu.ultra.data.repository.SettingsRepository
+import com.sukisu.ultra.data.repository.SettingsRepositoryImpl
+
+class SettingsViewModel(
+    private val repo: SettingsRepository = SettingsRepositoryImpl()
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(SettingsUiState())
+    val uiState: StateFlow<SettingsUiState> = _uiState.asStateFlow()
+
+    init {
+        refresh()
+    }
+
+    fun refresh() {
+        viewModelScope.launch {
+            val checkUpdate = repo.checkUpdate
+            val checkModuleUpdate = repo.checkModuleUpdate
+            val alternativeIcon = repo.alternativeIcon
+            val themeMode = repo.themeMode
+            val keyColor = repo.keyColor
+            val enablePredictiveBack = repo.enablePredictiveBack
+            val enableBlur = repo.enableBlur
+            val enableFloatingBottomBar = repo.enableFloatingBottomBar
+            val enableFloatingBottomBarBlur = repo.enableFloatingBottomBarBlur
+            val pageScale = repo.pageScale
+            val enableWebDebugging = repo.enableWebDebugging
+            val isLkmMode = repo.isLkmMode()
+
+            // Async loading for natives/features
+            val suCompatStatus = repo.getSuCompatStatus()
+            val suCompatPersistValue = repo.getSuCompatPersistValue()
+            val isSuEnabled = repo.isSuEnabled()
+
+            val suCompatMode = if (suCompatPersistValue == 0L) 2 else if (!isSuEnabled) 1 else 0
+
+            val kernelUmountStatus = repo.getKernelUmountStatus()
+            val isKernelUmountEnabled = repo.isKernelUmountEnabled()
+            val isDefaultUmountModules = repo.isDefaultUmountModules()
+
+            _uiState.update {
+                it.copy(
+                    checkUpdate = checkUpdate,
+                    checkModuleUpdate = checkModuleUpdate,
+                    alternativeIcon = alternativeIcon,
+                    themeMode = themeMode,
+                    keyColor = keyColor,
+                    enablePredictiveBack = enablePredictiveBack,
+                    enableBlur = enableBlur,
+                    enableFloatingBottomBar = enableFloatingBottomBar,
+                    enableFloatingBottomBarBlur = enableFloatingBottomBarBlur,
+                    pageScale = pageScale,
+                    enableWebDebugging = enableWebDebugging,
+                    suCompatStatus = suCompatStatus,
+                    suCompatMode = suCompatMode,
+                    isSuEnabled = isSuEnabled,
+                    kernelUmountStatus = kernelUmountStatus,
+                    isKernelUmountEnabled = isKernelUmountEnabled,
+                    isDefaultUmountModules = isDefaultUmountModules,
+                    isLkmMode = isLkmMode
+                )
+            }
+        }
+    }
+
+    fun setCheckUpdate(enabled: Boolean) {
+        repo.checkUpdate = enabled
+        _uiState.update { it.copy(checkUpdate = enabled) }
+    }
+
+    fun setCheckModuleUpdate(enabled: Boolean) {
+        repo.checkModuleUpdate = enabled
+        _uiState.update { it.copy(checkModuleUpdate = enabled) }
+    }
+    fun setAlternativeIcon(enabled: Boolean) {
+        repo.alternativeIcon = enabled
+        _uiState.update { it.copy(alternativeIcon = enabled) }
+    }
+
+    fun setThemeMode(mode: Int) {
+        repo.themeMode = mode
+        _uiState.update { it.copy(themeMode = mode) }
+    }
+
+    fun setKeyColor(color: Int) {
+        repo.keyColor = color
+        _uiState.update { it.copy(keyColor = color) }
+    }
+
+    fun setEnablePredictiveBack(enabled: Boolean) {
+        repo.enablePredictiveBack = enabled
+        _uiState.update { it.copy(enablePredictiveBack = enabled) }
+    }
+
+    fun setEnableBlur(enabled: Boolean) {
+        repo.enableBlur = enabled
+        _uiState.update { it.copy(enableBlur = enabled) }
+    }
+
+    fun setEnableFloatingBottomBar(enabled: Boolean) {
+        repo.enableFloatingBottomBar = enabled
+        _uiState.update { it.copy(enableFloatingBottomBar = enabled) }
+    }
+
+    fun setEnableFloatingBottomBarBlur(enabled: Boolean) {
+        repo.enableFloatingBottomBarBlur = enabled
+        _uiState.update { it.copy(enableFloatingBottomBarBlur = enabled) }
+    }
+
+    fun setPageScale(scale: Float) {
+        repo.pageScale = scale
+        _uiState.update { it.copy(pageScale = scale) }
+    }
+
+    fun setEnableWebDebugging(enabled: Boolean) {
+        repo.enableWebDebugging = enabled
+        _uiState.update { it.copy(enableWebDebugging = enabled) }
+    }
+
+    fun setSuCompatMode(mode: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+            when (mode) {
+                0 -> if (repo.setSuEnabled(true)) {
+                    repo.execKsudFeatureSave()
+                    repo.setSuCompatModePref(0)
+                    _uiState.update { it.copy(suCompatMode = 0, isSuEnabled = true) }
+                }
+
+                1 -> if (repo.setSuEnabled(true)) {
+                    repo.execKsudFeatureSave()
+                    if (repo.setSuEnabled(false)) {
+                        // "Disable until reboot" implies it should be enabled on next boot.
+                        // We set the preference to 0 (Enabled) to match the persistent state.
+                        repo.setSuCompatModePref(0)
+                        _uiState.update { it.copy(suCompatMode = 1, isSuEnabled = false) }
+                    }
+                }
+
+                2 -> if (repo.setSuEnabled(false)) {
+                    repo.execKsudFeatureSave()
+                    repo.setSuCompatModePref(2)
+                    _uiState.update { it.copy(suCompatMode = 2, isSuEnabled = false) }
+                }
+            }
+        }
+    }
+
+    fun setKernelUmountEnabled(enabled: Boolean) {
+        viewModelScope.launch(Dispatchers.IO) {
+            if (repo.setKernelUmountEnabled(enabled)) {
+                repo.execKsudFeatureSave()
+                _uiState.update { it.copy(isKernelUmountEnabled = enabled) }
+            }
+        }
+    }
+
+    fun setDefaultUmountModules(enabled: Boolean) {
+        viewModelScope.launch(Dispatchers.IO) {
+            if (repo.setDefaultUmountModules(enabled)) {
+                _uiState.update { it.copy(isDefaultUmountModules = enabled) }
+            }
+        }
+    }
+}

manager/app/src/main/java/com/sukisu/ultra/ui/viewmodel/SuperUserUiState.kt
@@ -1,0 +1,28 @@
+package com.sukisu.ultra.ui.viewmodel
+
+import androidx.compose.runtime.Immutable
+import com.sukisu.ultra.data.model.AppInfo
+import com.sukisu.ultra.ui.component.SearchStatus
+
+@Immutable
+data class GroupedApps(
+    val uid: Int,
+    val apps: List<AppInfo>,
+    val primary: AppInfo,
+    val anyAllowSu: Boolean,
+    val anyCustom: Boolean,
+    val shouldUmount: Boolean,
+    val ownerName: String? = null,
+)
+
+data class SuperUserUiState(
+    val isRefreshing: Boolean = false,
+    val appList: List<AppInfo> = emptyList(),
+    val groupedApps: List<GroupedApps> = emptyList(),
+    val userIds: List<Int> = emptyList(),
+    val searchStatus: SearchStatus = SearchStatus(""),
+    val searchResults: List<AppInfo> = emptyList(),
+    val showSystemApps: Boolean = false,
+    val showOnlyPrimaryUserApps: Boolean = false,
+    val error: Throwable? = null
+)

manager/app/src/main/java/com/sukisu/ultra/ui/viewmodel/TemplateUiState.kt
@@ -1,0 +1,10 @@
+package com.sukisu.ultra.ui.viewmodel
+
+import com.sukisu.ultra.data.model.TemplateInfo
+
+data class TemplateUiState(
+    val isRefreshing: Boolean = false,
+    val templates: List<TemplateInfo> = emptyList(),
+    val templateList: List<TemplateInfo> = emptyList(),
+    val error: Throwable? = null
+)

manager/app/src/main/java/com/sukisu/ultra/ui/webui/WebViewHelper.kt
@@ -20,8 +20,8 @@ import androidx.webkit.WebViewAssetLoader
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import com.sukisu.ultra.R
+import com.sukisu.ultra.data.repository.ModuleRepositoryImpl
 import com.sukisu.ultra.ui.util.createRootShell
-import com.sukisu.ultra.ui.viewmodel.ModuleViewModel
 import com.sukisu.ultra.ui.viewmodel.SuperUserViewModel
 import java.io.File
 
@@ -33,12 +33,9 @@ internal suspend fun prepareWebView(
     webUIState: WebUIState,
 ) {
     withContext(Dispatchers.IO) {
-        val viewModel = ModuleViewModel()
-        if (viewModel.moduleList.isEmpty()) {
-            viewModel.loadModuleList()
-        }
-
-        val moduleInfo = viewModel.moduleList.find { info -> info.id == moduleId }
+        val repo = ModuleRepositoryImpl()
+        val modules = repo.getModules().getOrDefault(emptyList())
+        val moduleInfo = modules.find { info -> info.id == moduleId }
 
         if (moduleInfo == null) {
             withContext(Dispatchers.Main) {
@@ -91,7 +88,12 @@ internal suspend fun prepareWebView(
                 .setDomain("mui.kernelsu.org")
                 .addPathHandler(
                     "/",
-                    SuFilePathHandler(activity, webRoot, shell, { webUIState.currentInsets }, { enable -> webUIState.isInsetsEnabled = enable })
+                    SuFilePathHandler(
+                        activity,
+                        webRoot,
+                        shell,
+                        { webUIState.currentInsets },
+                        { enable -> webUIState.isInsetsEnabled = enable })
                 )
                 .build()
 
@@ -134,7 +136,13 @@ internal suspend fun prepareWebView(
                     return true
                 }
 
-                override fun onJsPrompt(view: WebView?, url: String?, message: String?, defaultValue: String?, result: JsPromptResult?): Boolean {
+                override fun onJsPrompt(
+                    view: WebView?,
+                    url: String?,
+                    message: String?,
+                    defaultValue: String?,
+                    result: JsPromptResult?
+                ): Boolean {
                     if (message == null || result == null || defaultValue == null) return false
                     webUIState.uiEvent = WebUIEvent.ShowPrompt(message, defaultValue, result)
                     return true